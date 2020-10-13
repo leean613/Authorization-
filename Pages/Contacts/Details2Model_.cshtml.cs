04ddb5beef26ce3e1ed605a11b69e30fc3dbc731
an <lytruongan613@gmail.com>
2020-10-13 16:32:30 +0700

    Add project files.

---
+++ Pages/Contacts/Details2Model_.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactManager.Authorization;
+using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Pages.Contacts
+{
+    [AllowAnonymous]
+    public class Details2Model : DI_BasePageModel
+    {
+        public Details2Model(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
+        {
+        }
+
+        public Contact Contact { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Contact = await Context.Contact.FirstOrDefaultAsync(m => m.ContactId == id);
+
+            if (Contact == null)
+            {
+                return NotFound();
+            }
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Challenge();
+            }
+
+            var isAuthorized = User.IsInRole(Constants.ContactManagersRole) ||
+                               User.IsInRole(Constants.ContactAdministratorsRole);
+
+            var currentUserId = UserManager.GetUserId(User);
+
+            if (!isAuthorized
+                && currentUserId != Contact.OwnerID
+                && Contact.Status != ContactStatus.Approved)
+            {
+                return Forbid();
+            }
+
+            return Page();
+        }
+    }
+}
